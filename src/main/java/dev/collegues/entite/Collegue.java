23545350a0fc8094442649c6964f2d677711d132
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2020-02-04 15:20:43 +0100

    tp maj

--- src/main/java/dev/collegues/entite/Collegue.java
+++ src/main/java/dev/collegues/entite/Collegue.java
@@ -5,9 +5,9 @@ import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-import org.springframework.data.annotation.Id;
 
 @Entity
 public class Collegue {
