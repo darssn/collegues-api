d86b7c2df967fcecb9711cae75d5352066f9b114
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2020-02-07 14:21:05 +0100

    tp maj

--- src/main/java/dev/collegues/entite/PhotoWebApi.java
+++ src/main/java/dev/collegues/entite/PhotoWebApi.java
@@ -5,5 +5,41 @@ public class PhotoWebApi {
 	private String matricule;
 	private String photoUrl;
 	
+	public PhotoWebApi(){
+		
+	}
+
+	/**Getter
+	 * @return the matricule
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+
+	/**Setter
+	 * @param matricule the matricule to set
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	/**Getter
+	 * @return the photoUrl
+	 */
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	/**Setter
+	 * @param photoUrl the photoUrl to set
+	 */
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	
+	
+	
 	
 }
+
+
